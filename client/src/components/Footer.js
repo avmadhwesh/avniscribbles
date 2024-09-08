6b53006bfdf5f5fbfadcc6444da363658e8ae255
Avni Madhwesh <avmadhwesh@gmail.com>
2024-09-07 20:54:11 -0400

    Header recoloring; footer links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,10 +8,11 @@ const Footer = () => {
                 <p className="footer-text">© {new Date().getFullYear()} avniscribbles. All rights reserved.</p>
                 <ul className="footer-links">
                     <li><a href="https://yourwebsite.com/privacy">Privacy Policy</a></li>
-                    <li><a href="https://yourwebsite.com/terms">Terms of Service</a></li>
-                    <li><a href="https://yourwebsite.com/contact">Contact</a></li>
-                    <li><a href="https://twitter.com/yourhandle" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                    <li><a href="https://facebook.com/yourpage" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                    {/* <li><a href="https://yourwebsite.com/terms">Terms of Service</a></li> */}
+                    <li><a href="https://github.com/avmadhwesh/avniscribbles">Source Code</a></li>
+                    <li><a href="mailto:myemail@gmail.com" >Contact</a></li>
+                    <li><a href="https://instagram.com/avniscribbles/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li><a href="https://medium.com/@avniscribbles" target="_blank" rel="noopener noreferrer">Medium</a></li>
                     <ul className="footer-links">
                 </ul>
                 </ul>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,8 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">avniscribbles</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">

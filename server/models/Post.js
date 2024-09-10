53a1912ae17505ad95b0a4803378cd433f62e834
Avni Madhwesh <avmadhwesh@gmail.com>
2024-09-09 21:26:40 -0400

    Minor UI changes

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    type: { type: String, enum: ['blog', 'essay', 'creative'], required: true }
-}, 
+// const postSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     content: { type: String, required: true },
+//     type: { type: String, enum: ['blog', 'essay', 'creative'], required: true }
+// }, 
+const PostSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    type: String,
+    createdAt: { type: Date, default: Date.now },
+    dev: Boolean,
+    tags: [String],
+    notes: String,
+  
+    // New fields for pinning
+    pinned: { type: Boolean, default: false },   // Whether the post is pinned or not
+    pinnedOrder: { type: Number, default: null },  // Order in which the post should appear if pinned
+  },
 { 
     timestamps: true 
 });
